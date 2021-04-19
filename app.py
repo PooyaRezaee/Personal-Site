d26c6ab0efb4c5dff0eb11454d10c6e995dedfbf
Pooya <pooya.rezaee.official@gmail.com>
2021-04-19 01:52:29 -0700

    Make Blueprint admin and Register it

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# ----------- Views App ----------
+# =========== Views ===============
 from views import Home
+
+# =========== BluePrints ==========
+from mod_admin import admin
+
+app.register_blueprint(admin)
