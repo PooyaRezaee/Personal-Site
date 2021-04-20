002bed72f90f0091474d32b60c58cae0007510f2
Pooya <pooya.rezaee.official@gmail.com>
2021-04-20 12:05:39 -0700

    Set Validate for login to panel admin

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,10 +1,30 @@
 from . import admin
-from flask import render_template
+from flask import render_template,request,abort,flash,redirect,url_for,session
+from .forms import LoginForm
+from .models import Admin
 
 @admin.route('/')
 def Dashboard():
     return render_template('mod_admin/dashboard.html')
 
-@admin.route('/login/')
-def Login():
-    return render_template('mod_admin/login.html')
+@admin.route('/login/',methods=["GET"])
+def Login_get():
+    form = LoginForm()
+
+    return render_template('mod_admin/login.html',form=form)
+
+@admin.route('/login/',methods=["POST"])
+def Login_post():
+    form = LoginForm(request.form)
+
+    if not form.validate_on_submit():
+        flash("Form Not Validate")
+        return render_template('mod_admin/login.html',form=form)
+
+    admin_target = Admin.query.filter(Admin.UserName.ilike(f"{form.UserName.data}")).first()
+    if not admin_target or not admin_target.check_password(form.Password.data):
+        flash("UserName or Password is Wrong")
+        return render_template('mod_admin/login.html',form=form)
+    
+    session['permission'] = 'yes'
+    return redirect(url_for('admin.Dashboard'))
