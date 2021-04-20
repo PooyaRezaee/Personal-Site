c3bdb8179fb32cbac0ff2de329da2bdc027440f5
Pooya <pooya.rezaee.official@gmail.com>
2021-04-20 13:43:42 -0700

    import view logout

--- mod_admin/__init__.py
+++ mod_admin/__init__.py
@@ -6,6 +6,7 @@ admin = Blueprint("admin",__name__,url_prefix='/admin/')
 from .views import Dashboard
 from .views import Login_get
 from .views import Login_post
+from .views import logout
 
 # ============= Models ===========
 from .models import Admin
