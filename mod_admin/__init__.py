d1e15092771b21f5a3ec43176d9b16d496252a94
Pooya <pooya.rezaee.official@gmail.com>
2021-04-21 01:43:37 -0700

    Imported new Models

--- mod_admin/__init__.py
+++ mod_admin/__init__.py
@@ -10,3 +10,8 @@ from .views import logout
 
 # ============= Models ===========
 from .models import Admin
+from .models import Settings
+from .models import Skills
+from .models import Work_Sample
+from .models import Documents
+from .models import Contat_way
