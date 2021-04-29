f2bb97de0aa4923e02a3f0a509c98dff0a4abae6
Pooya <pooya.rezaee.official@gmail.com>
2021-04-29 11:56:18 -0700

    Dinamiced All data in Main Page

--- views.py
+++ views.py
@@ -1,6 +1,13 @@
 from app import app
 from flask import render_template
+from mod_admin.models import Settings,Skills,Work_Sample,Documents,Contat_way
 
 @app.route('/')
 def Home():
-    return render_template('home.html')
+    settings = Settings.query.first()
+    skills = Skills.query.all()
+    work_samples = Work_Sample.query.all()
+    documents = Documents.query.all()
+    contat_ways = Contat_way.query.all()
+
+    return render_template('home.html',settings=settings,skills=skills,work_samples=work_samples,documents=documents,contat_ways=contat_ways)
