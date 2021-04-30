aa2bad65fd3124ef54e3c9a2c891b0d356e902c3
Pooya <pooya.rezaee.official@gmail.com>
2021-04-30 10:05:40 -0700

    Add Set 'FullName' , 'AboutMe' , 'Background Image' and 'Profile Image' with admin panel

--- mod_admin/forms.py
+++ mod_admin/forms.py
@@ -1,7 +1,14 @@
-from wtforms import StringField,PasswordField
+from wtforms import StringField,PasswordField,TextAreaField,FileField
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     UserName = StringField(validators=[DataRequired()],render_kw={"placeholder":"User Name","class":"input-group filed"})
     Password = PasswordField(validators=[DataRequired()],render_kw={"placeholder":"Password","class":"input-group filed"})
+
+
+class SettingForms(FlaskForm):
+    FullName = StringField(validators=[DataRequired()],render_kw={"placeholder":"Your Name","class":"w-100 p-1 mb-4"})
+    profile_image = FileField(validators=[DataRequired()],render_kw={"class":"w-100 p-1 mb-4 border-0"})
+    background_image = FileField(validators=[DataRequired()],render_kw={"class":"w-100 p-1 mb-4 border-0"})
+    AboutME = TextAreaField(validators=[DataRequired()],render_kw={"placeholder":"About You","class":"w-100 p-1 mb-4"})
--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,15 +1,52 @@
 from . import admin
 from flask import render_template,request,abort,flash,redirect,url_for,session
-from .forms import LoginForm
-from .models import Admin
+from .forms import LoginForm,SettingForms
+from .models import Admin,Settings
+from app import db
 
-@admin.route('/')
+@admin.route('/',methods=["GET"])
 def Dashboard():
+    settings = Settings.query.first()
+    settingform = SettingForms(obj=settings)
+
     if session.get('permission') != "yes":
         flash("first Do Login !")
         return redirect(url_for('admin.Login_get'))
 
-    return render_template('mod_admin/dashboard.html')
+    return render_template('mod_admin/dashboard.html',setting_form=settingform)
+
+@admin.route('/<string:command>',methods=["POST"])
+def Commands(command):
+    settingform = SettingForms()
+
+    if settingform.validate_on_submit:
+        settings = Settings.query.first()
+
+        if command == "s-b":
+            settingform.background_image.data.save('static/images/background.jpg')
+            flash("Background Image Changed")
+
+        elif command == "s-p":
+            settingform.profile_image.data.save('static/images/profile.jpg')
+            flash("Profile Image Changed")
+
+        elif command == "ch-n":
+            settings.FullName = settingform.FullName.data
+            flash("You'r name Changed")
+
+        elif command == "ch-a":
+            settings.AboutME = settingform.AboutME.data
+            flash("About You Changed")
+        
+        try:
+            db.session.commit()
+        except :
+            flash("Error in Commit Data")
+
+        return redirect(url_for('admin.Dashboard'))
+
+    else:
+        return "form not validate"
 
 @admin.route('/login/',methods=["GET"])
 def Login_get():
